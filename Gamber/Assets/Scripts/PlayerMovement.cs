d65f188748969ff25c2f54e526fc57373d86f3f6
mydimons <mydimons.kb@gmail.com>
2025-04-07 22:01:32 -0700

    Template Movement Script
    THIS SCRIPT DOES NOTHING, just possibly some guidance for movement

---
+++ Gamber/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // v
+        // vvv
+        // if you want, DELETE THIS SCRIPT and do it yourself (maybe better for learning)
+        // BUT DONT FORGET to comment code ok-ish (describe what a function is doing),
+        // AND add nice, short, and descriptive commit messages (if there is more, add it to the commit description)
+        // ^^^
+        // ^
+
+
+        // use WASD to move UP, LEFT, DOWN, and RIGHT
+        // Adjustable variables for speed
+    }
+}
